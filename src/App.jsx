bbb6ca8190433f8711e1ed835eb72359186e295b
Hayzed-codes <raheemhorlah@gmail.com>
2024-06-11 16:51:13 +0100

    Ammend

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import Feedback from "./components/Feedback"
 import Contact from "./Contact"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-<<<<<<< HEAD
 import Mentor from "./components/Mentor"
-=======
 import Hero from "./components/Hero"
 import Omooro from "./components/Omooro"
->>>>>>> c50800a406d00f98b5c997f6668e6eadd513209a
 
 
 function App() {
@@ -15,17 +12,14 @@ function App() {
   return (
     <>
     <Header/>
-<<<<<<< HEAD
     <Mentor />
     <Footer/>
-=======
     <Hero />
     <Footer/>
     <Feedback/>
     <Contact/>
     <Omooro/>
     
->>>>>>> c50800a406d00f98b5c997f6668e6eadd513209a
     </>
   )
 }
